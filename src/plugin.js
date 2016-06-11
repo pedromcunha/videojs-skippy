e056277d15168225f4f2cde4ad5cace4dace9864
Pedro <pedro@younow.com>
2016-06-11 16:03:57 -0400

    add variables and breakage function

--- src/plugin.js
+++ src/plugin.js
@@ -3,6 +3,32 @@ import videojs from 'video.js';
 // Default options for the plugin.
 const defaults = {};
 
+let recordedTime = 0;
+let lastBrokeAt = false; //the last point of where the player broke, cleared at point of success
+let clearLastBrokeAt = false; //timeout that clears out the lastBrokeAt variable when playback continues without interruption
+let playlist; //playlist meta data for all segements within the HLS video
+let brokenSentinel = 0; //maxes the amount of breaks we allow for a video (if it's too high we dont want to go into an infite loop)
+let destroyEventHandler; //ensures that we don't register the destroy event twice
+let stuckStuck = 0; // check if the player gets stuck
+let lastCurrentTime = 0; // last time when the player got stuck
+let breakages = [];
+
+const checkForBreakage = (player) => {
+  //needs url
+	var breakHistory = breakages[player.src()];
+	let checkThisTime = Math.floor(recordedTime);
+	//if there's breakage at this time go ahead of it to the designated time
+	if (breakHistory && breakHistory[checkThisTime]) {
+		player.currentTime(breakHistory[checkThisTime]);
+	}
+	//clear out any remaining breakage queues
+	if (player.currentTime() > lastBrokeAt && !clearLastBrokeAt) {
+		clearLastBrokeAt = setTimeout(() => {
+			lastBrokeAt = false;
+		}, 500);
+	}
+}
+
 /**
  * Function to invoke when the player is ready.
  *
@@ -16,6 +42,26 @@ const defaults = {};
  */
 const onPlayerReady = (player, options) => {
   player.addClass('vjs-skippy');
+  player.on('timeupdate', (e) => {
+    if(player) {
+    	if (!lastBrokeAt && player.currentTime) {
+    		recordedTime = player.currentTime();
+    	}
+    	if (player.paused() && player.currentTime() === lastCurrentTime) {
+    		stuckStuck++;
+    		if (stuckStuck >= 5) {
+    			var skipTo = ((Math.floor(player.currentTime() / 2) + 1) * 2) + 0.1; // skip to next segment + 100ms buffer
+    			player.currentTime(skipTo);
+    			stuckStuck = 0;
+    		}
+    	} else {
+    		stuckStuck = 0;
+    	}
+    	lastCurrentTime = player.currentTime();
+    	checkForBreakage(player);
+    }
+  });
+
 };
 
 /**
@@ -40,6 +86,6 @@ const skippy = function(options) {
 videojs.plugin('skippy', skippy);
 
 // Include the version number.
-skippy.VERSION = '__VERSION__';
+skippy.VERSION = '0.0.1';
 
 export default skippy;
